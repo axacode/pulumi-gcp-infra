d218b09b4226327778895a0e86a25a4ec973eef8
Aravind <aravindashok@gmail.com>
2024-06-14 17:25:38 -0500

    setting force delete to true

--- dev/index.ts
+++ dev/index.ts
@@ -2,14 +2,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Define configuration variables
 const config = new pulumi.Config();
 const project = config.require("project");
 const region = config.require("region");
 const spannerInstanceName = config.require("spannerInstanceName");
 const spannerDatabaseName = config.require("spannerDatabaseName");
 
-// Create a Google Cloud Spanner instance
+
 const spannerInstance = new gcp.spanner.Instance(spannerInstanceName, {
     config: `regional-${region}`,
     displayName: spannerInstanceName,
@@ -18,7 +17,6 @@ const spannerInstance = new gcp.spanner.Instance(spannerInstanceName, {
     forceDestroy: true,
 });
 
-// Create a Google Cloud Spanner database
 const spannerDatabase = new gcp.spanner.Database(spannerDatabaseName, {
     instance: spannerInstance.name,
     project: project,
--- qa/index.ts
+++ qa/index.ts
@@ -2,22 +2,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Define configuration variables
 const config = new pulumi.Config();
 const project = config.require("project");
 const region = config.require("region");
 const spannerInstanceName = config.require("spannerInstanceName");
 const spannerDatabaseName = config.require("spannerDatabaseName");
 
-// Create a Google Cloud Spanner instance
+
 const spannerInstance = new gcp.spanner.Instance(spannerInstanceName, {
     config: `regional-${region}`,
     displayName: spannerInstanceName,
     numNodes: 1,
     project: project,
+    forceDestroy: true,
 });
 
-// Create a Google Cloud Spanner database
 const spannerDatabase = new gcp.spanner.Database(spannerDatabaseName, {
     instance: spannerInstance.name,
     project: project,
